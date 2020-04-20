35407045aafac3fa4ebdfe14108b505d4403839e
cadenmcmann <cadenmcmann@192.168.1.16>
2020-04-20 17:16:20 -0500

    Updated ImportScene

--- src/application/ImportScene.java
+++ src/application/ImportScene.java
@@ -1,9 +1,13 @@
 package application;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -30,12 +34,49 @@ public class ImportScene {
     root = new BorderPane();
 
     // Add a label to the root
-    root.setTop(new Label("Import Scene"));
-
+    Label topLabel = new Label("Import Scene");
+    BorderPane.setAlignment(topLabel, Pos.TOP_CENTER);
+    root.setTop(topLabel);
     // Add a button to the root
-    Button b = new Button("Go to Report Scene");
-    root.setCenter(b);
-    b.setOnAction(e -> {
+    
+    Button b1 = new Button("Generate Report");
+    BorderPane.setAlignment(b1, Pos.BOTTOM_RIGHT); // sets position of button
+    										      // to go to report scene
+    Button b2 = new Button("Add");
+    Button b3 = new Button("Import CSV");
+    BorderPane.setAlignment(b2, Pos.BOTTOM_RIGHT);
+    BorderPane.setAlignment(b3, Pos.BOTTOM_RIGHT);
+
+    
+    
+    
+    VBox vbox = new VBox();
+    vbox.setSpacing(20);
+    root.setRight(vbox); // creates a new Vbox
+
+    // adds labels and text boxes so user can enter in values for
+    // the farm, date, and weight 
+    TextField farmField = new TextField();
+    Label farm = new Label("Farm");
+    TextField dateField = new TextField();
+    Label date = new Label("Date");
+    TextField weightField = new TextField();
+    Label weight = new Label("Weight"); 
+    
+    // adds labels and text boxes to the vbox
+    vbox.getChildren().addAll(farm, farmField, date, dateField, weight, weightField, b2, b3, b1);
+    root.setRight(vbox); // sets the vbox to the right pane
+    
+    
+    // STILL NEEDS
+    // 1. Add table object to center of border pane
+    // 2. Add Hbox to top pane with "Farm, Date, Weight"
+    // 3. Add "report type" drop down 
+
+ 
+    // sets the action of button b to take user from ImportScene 
+    // to ReportScene
+    b1.setOnAction(e -> {
       Main.setStage("Report");
     });
 
--- src/application/ReportScene.java
+++ src/application/ReportScene.java
@@ -1,10 +1,17 @@
 package application;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.chart.*;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * The scene that occurs when a report is created from the import scene
@@ -16,31 +23,78 @@ public class ReportScene {
   // Main Pane containing the scene's content
   private static BorderPane root;
 
+  private class TableData {
+    SimpleStringProperty IDtype;
+    SimpleStringProperty total;
+    SimpleStringProperty percentofweight;
+
+    private TableData(String IDtype, String total, String percentofweight) {
+      this.IDtype = new SimpleStringProperty(IDtype);
+      this.total = new SimpleStringProperty(total);
+      this.percentofweight = new SimpleStringProperty(percentofweight);
+    }
+  }
+
   /**
    * Generates the elements for the report scene
    */
   private static void createScene() {
-
-    /**
-     * START WRITING CODE HERE: DELETE EXAMPLE CODE BELOW
-     */
-
     // Create an BorderPane as the root of the scene
     root = new BorderPane();
 
-    // Add a label to the root
-    root.setTop(new Label("Report Scene"));
-
     // Add a button to the root
     Button b = new Button("Go to Import Scene");
-    root.setCenter(b);
+    root.setBottom(b);
     b.setOnAction(e -> {
       Main.setStage("Import");
     });
+    FarmReport f = new FarmReport("annual");
+    int[] data = f.getTotal();
 
-    /**
-     * STOP WRITING CODE HERE
-     */
+    int[] percentages = determinePercentages(data);
+    String typeOfID = "Month";
+
+    // Display Type of Report at Top of screen
+    String report = new String("Farm Report");
+    Label l = new Label(report);
+    root.setTop(l);
+
+    // Display Table on Right side of screen
+    // General Format: Month/Farm ID, Total Weight, Percent of Total Weight
+    TableView table = new TableView();
+    table.setEditable(true);
+    TableColumn id = new TableColumn(typeOfID);
+    id.setCellValueFactory(new PropertyValueFactory<>("IDtype"));
+    TableColumn totalWeight = new TableColumn("Total Weight");
+    totalWeight.setCellValueFactory(new PropertyValueFactory<>("total"));
+    TableColumn percentOfWeight = new TableColumn("Percent of Total Weight");
+    percentOfWeight.setCellValueFactory(new PropertyValueFactory<>("percentofweight"));
+    ReportScene r = new ReportScene();
+    ObservableList<TableData> tdata = FXCollections.observableArrayList();
+    for (int i = 0; i < data.length; i++) {
+      String IDtype = Integer.toString(i);
+      String total = Integer.toString(data[i]);
+      String percentofweight = Integer.toString(percentages[i]);
+      TableData t = r.new TableData(IDtype, total, percentofweight);
+      tdata.add(t);
+    }
+    table.setItems(tdata);
+    table.getColumns().addAll(id, totalWeight, percentOfWeight);
+    root.setLeft(table);
+
+    // Display Pie Chart in Center
+    PieChart p = new PieChart();
+    // add data to piechart
+    for (int i = 0; i < percentages.length; i++) {
+      PieChart.Data slice = new PieChart.Data("Month " + Integer.toString(i), percentages[i]);
+      p.getData().add(slice);
+    }
+    ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+    p.setTitle(report);
+    root.setCenter(p);
+
+    // Display Min, Max, Average on Right side
+
   }
 
   /**
@@ -51,5 +105,49 @@ public class ReportScene {
     stage.setTitle(title);
     stage.setScene(new Scene(root, width, height));
     stage.show();
+  }
+
+  public static int[] determinePercentages(int[] month) {
+    int percent[] = new int[month.length];
+    int total = 0;
+    int percentage = 0;
+    for (int i = 0; i < month.length; i++) {
+      total = total + month[i];
+    }
+
+    for (int j = 0; j < month.length; j++) {
+      percentage = (int) ((((double) month[j] / (double) total)) * 100);
+      percent[j] = percentage;
+    }
+    return percent;
+  }
+
+  public static int getMin(int[] month) {
+    int min = month[0];
+    for (int i = 0; i < month.length; i++) {
+      if (min > month[i]) {
+        min = month[i];
+      }
+    }
+    return min;
+  }
+
+  public static int getMax(int[] month) {
+    int max = month[0];
+    for (int i = 0; i < month.length; i++) {
+      if (max < month[i]) {
+        max = month[i];
+      }
+    }
+    return max;
+  }
+
+  public static int getAverage(int[] month) {
+    int total = 0;
+    for (int i = 0; i < month.length; i++) {
+      total = total + month[i];
+    }
+    int average = (total / month.length);
+    return average;
   }
 }
